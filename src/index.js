f1c28a7bc20402e1a67d6b158313aad100d8d35c
Rehan Willemse <rehan.willemse@gmail.com>
2021-12-08 02:01:23 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const loader = document.querySelector('.loader');
 const main = document.querySelector('.main');
-const header = document.querySelector('.header');
-const nav = document.querySelector('.nav');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -22,8 +20,6 @@ function init() {
 
     main.style.display = 'block';
     setTimeout(() => (main.style.opacity = 1), 50);
-    setTimeout(() => (header.style.opacity = 1), 50);
-    setTimeout(() => (nav.style.opacity = 1), 50);
   }, 2000);
 }
 
